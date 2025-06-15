e0186f9cbdf5a923258669adc6c78bcf6d3d56c2
SonSyntax <sonyhidayatullah55@gmail.com>
2025-06-15 19:47:49 +0700

    Update model_eoq.py

--- model_eoq.py
+++ model_eoq.py
@@ -1,5 +1,17 @@
-
-import numpy as np
+import math
 
 def hitung_eoq(D, S, H):
-    return np.sqrt((2 * D * S) / H)
+    # EOQ Formula
+    eoq = math.sqrt((2 * D * S) / H)
+
+    # Frekuensi pemesanan
+    frekuensi = D / eoq
+
+    # Total biaya: penyimpanan + pemesanan
+    biaya_total = (eoq / 2) * H + (D / eoq) * S
+
+    return {
+        "EOQ": round(eoq, 2),
+        "Frekuensi": round(frekuensi, 2),
+        "Total_Biaya": round(biaya_total, 2)
+    }
