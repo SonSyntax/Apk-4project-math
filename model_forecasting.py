a1fb4823db67a19c898c71299cdf45c595a34180
SonSyntax <sonyhidayatullah55@gmail.com>
2025-06-15 19:49:23 +0700

    Update model_forecasting.py

--- model_forecasting.py
+++ model_forecasting.py
@@ -1,20 +1,9 @@
-
 import numpy as np
-import pandas as pd
-from sklearn.linear_model import LinearRegression
 
-def forecasting(data: pd.DataFrame):
-    # data harus memiliki kolom 'bulan' dan 'permintaan'
-    data = data.copy()
-    data['bulan'] = np.arange(1, len(data) + 1).reshape(-1, 1)
-    model = LinearRegression()
-    model.fit(data[['bulan']], data['permintaan'])
-
-    # Prediksi 3 bulan ke depan
-    bulan_prediksi = np.arange(len(data) + 1, len(data) + 4).reshape(-1, 1)
-    prediksi = model.predict(bulan_prediksi)
-    hasil = pd.DataFrame({
-        'Bulan': ['Bulan ke-' + str(i) for i in range(len(data)+1, len(data)+4)],
-        'Prediksi Permintaan': np.round(prediksi, 2)
-    })
-    return hasil
+def forecast_regresi(data_bulanan, bulan_ke):
+    x = np.arange(1, len(data_bulanan) + 1)
+    y = np.array(data_bulanan)
+    coef = np.polyfit(x, y, 1)
+    a, b = coef
+    prediksi = a * bulan_ke + b
+    return round(prediksi, 2), a, b
